04106049ee256d576283753c99ea32004ce1ba33
Jack Grigg <thestr4d@gmail.com>
2024-01-10 17:54:05 +0000

    Conditionally compile Apple-specific APIs
    This enables development on non-Apple machines, and potentially in
    future usage of the Swift SDK with non-Apple targets.

--- rust/wrapper.h
+++ rust/wrapper.h
@@ -1,3 +1,4 @@
+#if defined(__APPLE__) && defined(__MACH__)
 #include <os/log.h>
 #include <os/signpost.h>
 
@@ -5,3 +6,4 @@ void os_log_with_type_rs(os_log_t log, os_log_type_t type, const char *message);
 
 void os_signpost_interval_begin_rs(os_log_t log, os_signpost_id_t interval_id, const char *label);
 void os_signpost_interval_end_rs(os_log_t log, os_signpost_id_t interval_id, const char *label);
+#endif

1567df6e2ab17fa6568ca598ad836decaad773c1
Jack Grigg <jack@electriccoin.co>
2025-02-15 00:16:47 +1300

    Add support for fetching and submitting transactions over Tor

--- rust/src/tor.rs
+++ rust/src/tor.rs
@@ -2,8 +2,14 @@
 
 use std::path::Path;
 
+use anyhow::anyhow;
+use tonic::transport::{Channel, Uri};
 use tor_rtcompat::{BlockOn, PreferredRuntime};
-use zcash_client_backend::tor::Client;
+use zcash_client_backend::{
+    proto::service::{self, compact_tx_streamer_client::CompactTxStreamerClient},
+    tor::Client,
+};
+use zcash_protocol::TxId;
 
 pub struct TorRuntime {
     runtime: PreferredRuntime,
@@ -49,6 +55,69 @@ impl TorRuntime {
         Self {
             runtime: self.runtime.clone(),
             client: self.client.isolated_client(),
+        }
+    }
+
+    /// Connects to the lightwalletd server at the given endpoint.
+    ///
+    /// Each connection returned by this method is isolated from any other Tor usage.
+    pub(crate) fn connect_to_lightwalletd(&self, endpoint: Uri) -> anyhow::Result<LwdConn> {
+        let Self { runtime, client } = self.isolated_client();
+
+        let conn = runtime.block_on(async { client.connect_to_lightwalletd(endpoint).await })?;
+
+        Ok(LwdConn {
+            runtime,
+            _client: client,
+            conn,
+        })
+    }
+}
+
+pub struct LwdConn {
+    conn: CompactTxStreamerClient<Channel>,
+    _client: Client,
+    runtime: PreferredRuntime,
+}
+
+impl LwdConn {
+    /// Fetches the transaction with the given ID.
+    pub(crate) fn get_transaction(&mut self, txid: TxId) -> anyhow::Result<Vec<u8>> {
+        let request = service::TxFilter {
+            hash: txid.as_ref().to_vec(),
+            ..Default::default()
+        };
+
+        let response = self
+            .runtime
+            .clone()
+            .block_on(async { self.conn.get_transaction(request).await })?
+            .into_inner();
+
+        Ok(response.data)
+    }
+
+    /// Submits a transaction to the Zcash network.
+    pub(crate) fn send_transaction(&mut self, tx_bytes: Vec<u8>) -> anyhow::Result<()> {
+        let request = service::RawTransaction {
+            data: tx_bytes,
+            ..Default::default()
+        };
+
+        let response = self
+            .runtime
+            .clone()
+            .block_on(async { self.conn.send_transaction(request).await })?
+            .into_inner();
+
+        if response.error_code == 0 {
+            Ok(())
+        } else {
+            Err(anyhow!(
+                "Failed to submit transaction ({}): {}",
+                response.error_code,
+                response.error_message
+            ))
         }
     }
 }
